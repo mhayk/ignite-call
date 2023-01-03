8e4c250fc00d276d74970cf46ae6215811ca84f3
Mhayk Whandson <hi@mhayk.com>
2023-01-03 09:10:31 +0000

    feat: create the form validation with zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,15 +1,56 @@
-import { Button, TextInput } from '@ignite-ui/react'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form } from './styles'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Username requires min 3 letters.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'The username can contains only letters and hifens.',
+    })
+    .transform((username) => username.toLocaleLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size={'sm'} prefix="mhayk.com/" placeholder="your-username" />
-      <Button size={'sm'} type="submit">
-        Book
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size={'sm'}
+          prefix="mhayk.com/"
+          placeholder="your-username"
+          {...register('username')}
+        />
+        <Button size={'sm'} type="submit">
+          Book
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Enter the desired username.'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
