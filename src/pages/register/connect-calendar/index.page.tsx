67a826d8791ba08b13adb10afcfbf49941c2d488
Mhayk whandson <hi@mhayk.com>
2023-01-09 16:30:38 +0000

    feat: create the page connect calendar

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,35 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function Register() {
+  //   async function handleRegister(data: RegisterFormData) {}
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Connect your agenda !</Heading>
+        <Text>
+          Connect your calendar to automatically check busy hours and new events
+          as they are scheduled.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Connect
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+        <Button type="submit">
+          Next step
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
